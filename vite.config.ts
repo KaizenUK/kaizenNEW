9956363f13c36c62c16520758596670cc163858a
SeanMcDonnell83 <sean.mcdonnell1983@gmail.com>
2026-02-10 02:32:18 +0000

    reactsnap pilot

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     outDir: "dist/spa",
+    target: mode === "snap" ? "chrome73" : undefined,
     modulePreload: {
       polyfill: false,
     },
