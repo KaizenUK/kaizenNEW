3ed35a0efcc6654f5d9d60b4f13631adca6f5e67
Kaizen <sean@kaizenweb.co.uk>
2026-03-05 04:23:18 +0000

    phase 9

--- client/static/RoutedReactPage.tsx
+++ client/static/RoutedReactPage.tsx
@@ -1,14 +1,6 @@
 import { HelmetProvider } from "@/lib/helmet";
 import { CalendlyProvider } from "@/context/CalendlyContext";
 import { RoutePathProvider } from "@/components/routing/RoutePathContext";
-import WebDesignLiverpoolPage from "@/pages/services/WebDesignLiverpool";
-import WebDesignChesterPage from "@/pages/services/WebDesignChester";
-import WebDesignWarringtonPage from "@/pages/services/WebDesignWarrington";
-import WebDesignWirralPage from "@/pages/services/WebDesignWirral";
-import LocalSeoPage from "@/pages/services/LocalSeo";
-import EcommercePage from "@/pages/services/Ecommerce";
-import WordPressWebDesignPage from "@/pages/services/WordPressWebDesign";
-import DigitalTransformationPage from "@/pages/services/DigitalTransformation";
 import AsCollectionsPage from "@/pages/caseStudies/AsCollections";
 import HelenMooreHairdressingPage from "@/pages/caseStudies/HelenMooreHairdressing";
 import IndependentRetailerPage from "@/pages/caseStudies/IndependentRetailer";
@@ -21,14 +13,6 @@ type RoutedReactPageProps = {
 };
 
 const PAGE_COMPONENTS_BY_PATH: Record<string, React.ComponentType> = {
-  "/web-design-liverpool": WebDesignLiverpoolPage,
-  "/web-design-chester": WebDesignChesterPage,
-  "/web-design-warrington": WebDesignWarringtonPage,
-  "/web-design-wirral": WebDesignWirralPage,
-  "/services/local-seo": LocalSeoPage,
-  "/services/ecommerce": EcommercePage,
-  "/services/wordpress-web-design": WordPressWebDesignPage,
-  "/digital-transformation": DigitalTransformationPage,
   "/case-studies/as-collections": AsCollectionsPage,
   "/case-studies/helen-moore-hairdressing": HelenMooreHairdressingPage,
   "/case-studies/independent-retailer": IndependentRetailerPage,
